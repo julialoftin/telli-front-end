6468568f8fced139d1a45085e859c3ac63a41ddb
Julia Loftin <julia.loftin7@gmail.com>
2024-01-26 20:19:38 -0600

    reformats using prettier

--- telli-react/src/components/AddMediaItemToWatchListSelect.tsx
+++ telli-react/src/components/AddMediaItemToWatchListSelect.tsx
@@ -7,18 +7,22 @@ interface MediaItemDTO {
 }
 
 interface WatchList {
-    id: number;
-    name: string;
-    description: string;
+  id: number;
+  name: string;
+  description: string;
 }
 
 interface AddMediaToWatchListProps {
-    mediaItemDTO: MediaItemDTO;
+  mediaItemDTO: MediaItemDTO;
 }
 
-export default function AddMediaToWatchListSelect({ mediaItemDTO }: AddMediaToWatchListProps) {
+export default function AddMediaToWatchListSelect({
+  mediaItemDTO,
+}: AddMediaToWatchListProps) {
   const [watchLists, setWatchLists] = useState<WatchList[]>([]);
-  const [selectedWatchList, setSelectedWatchList] = useState<number | null>(null);
+  const [selectedWatchList, setSelectedWatchList] = useState<number | null>(
+    null
+  );
 
   useEffect(() => {
     async function fetchWatchLists() {
@@ -57,20 +61,26 @@ export default function AddMediaToWatchListSelect({ mediaItemDTO }: AddMediaToWa
 
   return (
     <>
-        <div>
-            <form>
-                <select onChange={(event) => setSelectedWatchList(Number(event.target.value))}>
-                    <option value="" disabled>Select a Watch List</option>
-                    {watchLists.map((watchList) => (
-                        <option key={watchList.id} value={watchList.id}>
-                            {watchList.name}
-                        </option>
-                    ))}
-                </select>
+      <div>
+        <form>
+          <select
+            onChange={(event) =>
+              setSelectedWatchList(Number(event.target.value))
+            }
+          >
+            <option value="" disabled>
+              Select a Watch List
+            </option>
+            {watchLists.map((watchList) => (
+              <option key={watchList.id} value={watchList.id}>
+                {watchList.name}
+              </option>
+            ))}
+          </select>
 
-                <button type="button" onClick={handleAddToWatchList}></button>
-            </form>
-        </div>
+          <button type="button" onClick={handleAddToWatchList}></button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
