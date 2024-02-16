0abadbd76aa2ae5118dac4755742168f9e5bd8c9
Angela Engle <angelaengle67@gmail.com>
2024-02-15 18:41:39 -0600

    Updated Search

--- telli-react/src/components/Search.tsx
+++ telli-react/src/components/Search.tsx
@@ -1,18 +1,32 @@
 import React, { useEffect, useState } from "react";
+import { ChangeEvent } from 'react';
 
 
 export const SearchBar = () => {
     const [search, setSearch] = useState("")
-    const [searchData, setSearchData] = useState("")
 
-const handleChange = (event) => {
-    setSearch(event.target.value)
-}
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value);
+    };
 
 useEffect(()=> {
     if (search !== ""){
-        fetch("https://api.themoviedb.org/3/movie/popular?api_key=${fae46c29ceeaecf4154856d4ac887a0a}").then((response) => 
-        response.json()).then((json)=>{console.log(json);})
+        const url = (
+            'https://api.themoviedb.org/3/search/movie?' +
+            new URLSearchParams({search}).toString() + 'include_adult=false&language=en-US&page=1'
+          );
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZGU2NTJhODNlYzkwNzcxZTdkZWQ4YWEwMmMyMDJkYSIsInN1YiI6IjY1YWQ1OWJiMTU4Yzg1MDBhYzFiZTEyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nA96D4cB1ZUGNw42oU3Ah6NVMuQhb2WTlIehZJskGDg'
+            }
+          };
+          
+          fetch(url, options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
     }
         }, [search])
 
@@ -21,10 +35,10 @@ return (
             <div className="input-wrapper">Search Bar
             <input placeholder="Type to search" 
             
-            onChange = {handleChange}
+            onChange = {handleInputChange}
             value= {search}
             ></input>
-
+        
             </div>
 )
 }
