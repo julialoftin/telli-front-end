715d50c4c45ed0c6105994fa75ed53ed62d7b433
Julia Loftin <julia.loftin7@gmail.com>
2024-02-19 22:58:09 -0600

    spacing between media feed elements

--- telli-react/src/components/MediaFeed.tsx
+++ telli-react/src/components/MediaFeed.tsx
@@ -52,11 +52,11 @@ export default function MediaFeed() {
   return (
     <>
       <h1 className="movie-feed-title">Popular Movies</h1>
-      <div className="movie-list">
+      <div className="movie-list-media-feed">
         {popularMovies.map((movie) => (
-          <div key={movie.id} className="movie-item">
+          <div key={movie.id} className="movie-item-media-feed">
             <a href={`/movie/${movie.id}`}>
-              <img
+              <img className="media-feed"
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
               />
