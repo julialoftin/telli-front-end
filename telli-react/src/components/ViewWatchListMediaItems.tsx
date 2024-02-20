484ad962121178e58586321ca1a1e2b2804718c7
Julia Loftin <julia.loftin7@gmail.com>
2024-02-19 19:39:13 -0600

    removes unused import

--- telli-react/src/components/ViewWatchListMediaItems.tsx
+++ telli-react/src/components/ViewWatchListMediaItems.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { WatchList } from "./ViewWatchLists";
 import DeleteMediaItemFromWatchListButton from "./DeleteMediaItemFromWatchListButton";
