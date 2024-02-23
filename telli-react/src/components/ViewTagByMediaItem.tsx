edd97e5e2f1ae9923533b210edc9dd86b663a3dc
Julia Loftin <julia.loftin7@gmail.com>
2024-02-23 17:43:25 -0600

    updates components that call back end api

--- telli-react/src/components/ViewTagByMediaItem.tsx
+++ telli-react/src/components/ViewTagByMediaItem.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 interface TagDTO {
   name: string;
@@ -16,44 +16,53 @@ interface MediaItemDTO {
 }
 
 interface ViewTagByMediaItemProps {
-    mediaItemDTO: MediaItemDTO;
+  mediaItemDTO: MediaItemDTO;
 }
 
-export default function ViewTagByMediaItem({mediaItemDTO}: ViewTagByMediaItemProps) {
-    const [tags, setTags] = useState<Tag[]>([]);
+export default function ViewTagByMediaItem({
+  mediaItemDTO,
+}: ViewTagByMediaItemProps) {
+  const [tags, setTags] = useState<Tag[]>([]);
 
-    useEffect (() => {
-        const fetchTagsByMediaItem = async () => {
-            try {
-                const response = await fetch(`http://localhost:8080/api/tag/get-tags-by-media-item/${mediaItemDTO.tmdbId}`, {
-                    method: 'GET',
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    credentials: "include",
-                });
-                const result = await response.json();
-                setTags(result)
-                console.log(result);
-                return result;
-            } catch (error) {
-                console.error("Error fetching tag by media item: ", error);
-            }
-        };
-        fetchTagsByMediaItem();
-    }, []);
+  useEffect(() => {
+    const fetchTagsByMediaItem = async () => {
+        const queryParams = new URLSearchParams({
+            tmdbId: mediaItemDTO.tmdbId.toString(),
+            mediaType: mediaItemDTO.mediaType,
+          });
+      try {
+        const response = await fetch(
+          `http://localhost:8080/api/tag/get-tags-by-media-item?${queryParams}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          }
+        );
+        const result = await response.json();
+        setTags(result);
+        // console.log(result);
+        return result;
+      } catch (error) {
+        console.error("Error fetching tag by media item: ", error);
+      }
+    };
+    fetchTagsByMediaItem();
+  }, []);
 
-    return (
-        <>
-            <div>
-                <ul>
-                    {tags.map((tag) => (
-                        <li key={tag.id}>
-                            <Link to={`/media-item/${tag.id}`}>{tag.name}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <ul>
+          {tags.map((tag) => (
+            <li key={tag.id}>
+              <Link to={`/media-item/${tag.id}`}>{tag.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
